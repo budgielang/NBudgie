bd07d5b7bfec1a0f55551de7c227bb2c36bd8545
Josh Goldberg <josh@fullscreenmario.com>
2017-07-16 04:29:43 -0700

    Added math min command

---
+++ src/commands/en-us/math min/matchers.ts
@@ -0,0 +1,17 @@
+import { IMatchersList } from "../../../matchers";
+import { RegExpMatchTest } from "../../../matchTests/regExpMatchTest";
+import { ICommandArgs } from "./command";
+
+export class MatchersList implements IMatchersList {
+    public readonly matchers = [
+        {
+            test: new RegExpMatchTest(/(lesser|least|smaller|smallest) (between|from|of) (.+) (and|or) (.+)/i),
+            parseArgs(matches: RegExpMatchArray): ICommandArgs {
+                return {
+                    left: matches[3],
+                    right: matches[5],
+                };
+            },
+        },
+    ];
+}
