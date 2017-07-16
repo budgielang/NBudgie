80b57bc57d377d571eb205f67317a33b6e46a1b0
Josh Goldberg <josh@fullscreenmario.com>
2017-07-16 04:17:11 -0700

    Added member variable command

---
+++ src/commands/en-us/member variable/command.ts
@@ -0,0 +1,19 @@
+import { ICommand } from "../../../command";
+
+export interface ICommandArgs {
+    instanceName: string;
+    privacy: string;
+    variableName: string;
+}
+
+export class Command implements ICommand<ICommandArgs> {
+    public render(args: ICommandArgs) {
+        const { instanceName, privacy, variableName } = args;
+
+        return {
+            lines: [
+                [`member variable : ${privacy} ${instanceName} ${variableName}`],
+            ],
+        };
+    }
+}
