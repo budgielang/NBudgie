dfc7b65ce275dfe04befe6663878d76a6f391b34
Josh Goldberg <josh@fullscreenmario.com>
2017-07-16 04:26:16 -0700

    Added math max command

---
+++ src/commands/en-us/math max/matchers.ts
@@ -0,0 +1,17 @@
+import { IMatchersList } from "../../../matchers";
+import { RegExpMatchTest } from "../../../matchTests/regExpMatchTest";
+import { ICommandArgs } from "./command";
+
+export class MatchersList implements IMatchersList {
+    public readonly matchers = [
+        {
+            test: new RegExpMatchTest(/(bigger|biggest|greater|greatest|larger|largest|) (between|from|of) (.+) (and|or) (.+)/i),
+            parseArgs(matches: RegExpMatchArray): ICommandArgs {
+                return {
+                    left: matches[3],
+                    right: matches[5],
+                };
+            },
+        },
+    ];
+}
