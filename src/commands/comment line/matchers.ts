1a351774a1b6d0a9214f4129cee9bdf4da3c0a78
Josh Goldberg <josh@fullscreenmario.com>
2017-07-16 09:41:22 -0700

    Added some flexibility to comments

--- src/commands/comment line/matchers.ts
+++ src/commands/comment line/matchers.ts
@@ -5,7 +5,15 @@ import { ICommandArgs } from "./command";
 export class MatchersList implements IMatchersList {
     public readonly matchers = [
         {
-            test: new RegExpMatchTest(/comment (noting|saying|that) (.+)/i),
+            test: new RegExpMatchTest(/(comment|note) (noting|saying|that) (.+)/i),
+            parseArgs(matches: RegExpMatchArray): ICommandArgs {
+                return {
+                    word: matches[3],
+                };
+            },
+        },
+        {
+            test: new RegExpMatchTest(/(comment|note): (.+)/i),
             parseArgs(matches: RegExpMatchArray): ICommandArgs {
                 return {
                     word: matches[2],
