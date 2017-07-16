4f711c7b8bd839651dfa3a9e634655529f36d35c
Josh Goldberg <josh@fullscreenmario.com>
2017-07-15 21:20:29 -0700

    Added more operation options

--- src/commands/en-us/operation/matchers.ts
+++ src/commands/en-us/operation/matchers.ts
@@ -6,12 +6,67 @@ export class MatchersList implements IMatchersList {
     public readonly matchers = [
         {
             onlyDeep: true,
-            test: new RegExpMatchTest(/(.+) is equal to (.+)/),
+            test: new RegExpMatchTest(/(.+) (is not|isn't) equal to (.+)/),
+            parseArgs(matches: RegExpMatchArray): ICommandArgs {
+                return {
+                    item: matches[1],
+                    operator: "(not equal to)",
+                    value: matches[3],
+                };
+            },
+        },
+        {
+            onlyDeep: true,
+            test: new RegExpMatchTest(/(.+) (is equal to|equals) (.+)/),
             parseArgs(matches: RegExpMatchArray): ICommandArgs {
                 return {
                     item: matches[1],
                     operator: "(equal to)",
-                    value: matches[2],
+                    value: matches[3],
+                };
+            },
+        },
+        {
+            onlyDeep: true,
+            test: new RegExpMatchTest(/(.+) is (greater than|over) or equal to (.+)/),
+            parseArgs(matches: RegExpMatchArray): ICommandArgs {
+                return {
+                    item: matches[1],
+                    operator: "(greater than or equal to)",
+                    value: matches[3],
+                };
+            },
+        },
+        {
+            onlyDeep: true,
+            test: new RegExpMatchTest(/(.+) is (greater than|over) (.+)/),
+            parseArgs(matches: RegExpMatchArray): ICommandArgs {
+                return {
+                    item: matches[1],
+                    operator: "(greater than)",
+                    value: matches[3],
+                };
+            },
+        },
+        {
+            onlyDeep: true,
+            test: new RegExpMatchTest(/(.+) is (less than|under) or equal to (.+)/),
+            parseArgs(matches: RegExpMatchArray): ICommandArgs {
+                return {
+                    item: matches[1],
+                    operator: "(less than or equal to)",
+                    value: matches[3],
+                };
+            },
+        },
+        {
+            onlyDeep: true,
+            test: new RegExpMatchTest(/(.+) is (less than|under) (.+)/),
+            parseArgs(matches: RegExpMatchArray): ICommandArgs {
+                return {
+                    item: matches[1],
+                    operator: "(less than)",
+                    value: matches[3],
                 };
             },
         },
