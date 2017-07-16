4adec54a61c102ad220a11aba165f11200116c49
Josh Goldberg <josh@fullscreenmario.com>
2017-07-16 04:29:09 -0700

    Removed empty case from math max

--- src/commands/en-us/math max/matchers.ts
+++ src/commands/en-us/math max/matchers.ts
@@ -5,7 +5,7 @@ import { ICommandArgs } from "./command";
 export class MatchersList implements IMatchersList {
     public readonly matchers = [
         {
-            test: new RegExpMatchTest(/(bigger|biggest|greater|greatest|larger|largest|) (between|from|of) (.+) (and|or) (.+)/i),
+            test: new RegExpMatchTest(/(bigger|biggest|greater|greatest|larger|largest) (between|from|of) (.+) (and|or) (.+)/i),
             parseArgs(matches: RegExpMatchArray): ICommandArgs {
                 return {
                     left: matches[3],
